ab9369c818aa2f1eed5f6bd9c93366cdc7ae6969
Ziv Lifshits <zlifshit@cisco.com>
2017-03-19 21:19:45 +0200

    Added comments

--- Lotto.py
+++ Lotto.py
@@ -2,6 +2,7 @@ import csv
 import random
 lotto_db = [];
 
+# Reads data from Israeli Lottery csv file, parses it and calculates sum and average
 def read_lotto_file(depth=-1):
     input_file = csv.DictReader(open("Lotto.csv"))
     for row in input_file:
@@ -14,10 +15,17 @@ def read_lotto_file(depth=-1):
             row['fullavg'] = row['fullsum'] / 7
             lotto_db.append(row)
 
+# Prints database
+def dump_db(lotto_db):
+    for row in lotto_db:
+         # if (int(row['1'])>37 or int(row['2'])>37 or int(row['3'])>37 or int(row['4'])>37 or int(row['5'])>37 or int(row['6'])>37):
+         print row['Game'], row['Date'], row['numbers'], row['Additional number'], row['sum']
+
+# Returns a list of most recent numbers order
 def most_recent_number(lotto_db):
     most_recent = {}
     for row in lotto_db:
-        for i in range(1,6):
+        for i in range(1,7):
             if most_recent.get(row[str(i)]):
                 most_recent[row[str(i)]] += 1
             else:
@@ -25,8 +33,10 @@ def most_recent_number(lotto_db):
 
     return most_recent
 
+# Returns a list of numbers per colomn
 def number_ranges(lotto_db):
     num_ranges = {};
+    num_ranges['Additional number'] = [int(row['Additional number']) for row in lotto_db]
     for i in range(1,7):
         num_ranges[str(i)] = {};
         num_ranges[str(i)]['min']=99;
@@ -39,44 +49,38 @@ def number_ranges(lotto_db):
         num_ranges[str(i)]['avg'] = sum(num_ranges[str(i)]['list'])/len(num_ranges[str(i)]['list'])
     return num_ranges
 
-def dump_db(lotto_db):
-    for row in lotto_db:
-        #if (int(row['1'])>37 or int(row['2'])>37 or int(row['3'])>37 or int(row['4'])>37 or int(row['5'])>37 or int(row['6'])>37):
-        print row['Game'],row['Date'],row['numbers'],row['Additional number'],row['sum']
 
-def generate_results(res_num):
+def generate_results(number_ranges):
     random.seed()
     result = {}
-    for j in range(1, res_num):
-        result[str(j)] = {}
-        result[str(j)]['list'] = []
-        for i in range(1, 7):
-            candidate = number_ranges(lotto_db)[str(i)]['list'][
-                random.randint(1, len(number_ranges(lotto_db)[str(i)]['list']))]
-            while candidate  in result[str(j)]['list']:
-                candidate = number_ranges(lotto_db)[str(i)]['list'][
-                    random.randint(1, len(number_ranges(lotto_db)[str(i)]['list']))]
-            result[str(j)]['list'].append(candidate)
-        result[str(j)]['list'] = sorted(result[str(j)]['list'])
-        result[str(j)]['sum'] = sum(result[str(j)]['list'])
-        result[str(j)]['avg'] = result[str(j)]['sum'] / len(result[str(j)]['list'])
-        print result[str(j)]
+    result['list'] = []
+    for i in range(1, 7):
+        rrand = random.randint(0, len(number_ranges[str(i)]['list'])-1)
+        candidate = number_ranges[str(i)]['list'][rrand]
+        while candidate  in result['list']:
+            candidate = number_ranges[str(i)]['list'][
+                random.randint(0, len(number_ranges[str(i)]['list'])-1)]
+        result['list'].append(candidate)
+    result['list'] = sorted(result['list'])
+    result['sum'] = sum(result['list'])
+    result['avg'] = result['sum'] / len(result['list'])
+    result['Additional number'] = number_ranges['Additional number'][random.randint(0, len(number_ranges['Additional number'])-1)]
     return result
 
 def generate_probability_results(res_num):
     random.seed()
     result = {}
     plist = []
-    for i in range(1,6):
+    for i in range(1,7):
         plist += [item for item in number_ranges(lotto_db)[str(i)]['list']]
 
-    for j in range(1, res_num):
+    for j in range(1, res_num+1):
         result[str(j)] = {}
         result[str(j)]['list'] = []
         for i in range(1, 7):
-            candidate = plist[random.randint(1, len(plist))]
+            candidate = plist[random.randint(0, len(plist)-1)]
             while candidate in result[str(j)]['list']:
-                candidate = plist[random.randint(1, len(plist))]
+                candidate = plist[random.randint(0, len(plist)-1)]
             result[str(j)]['list'].append(candidate)
         result[str(j)]['list'] = sorted(result[str(j)]['list'])
         result[str(j)]['sum'] = sum(result[str(j)]['list'])
@@ -84,6 +88,7 @@ def generate_probability_results(res_num):
         print result[str(j)]
     return result
 
+# Returns list of sum by probability (desc)
 def sum_probability(lotto_db):
     prob_sum = {}
     for row in lotto_db:
@@ -94,6 +99,7 @@ def sum_probability(lotto_db):
     print sorted(prob_sum, key=prob_sum.get, reverse=True)
     return prob_sum
 
+# Returns normal distribution of sums
 def normal_sum_dist():
     avg_sum = {}
     for n1 in range(1,33):
@@ -110,43 +116,60 @@ def normal_sum_dist():
                                 #print n1, n2, n3, n4, n5, n6, str(n1+n2+n3+n4+n5+n6)
     print sorted(avg_sum, key=avg_sum.get, reverse=True)
 
+# Finds if there are 2 following numbers (1,2 or 11,12)
 def two_following(numbers):
     for num in numbers:
         if num+1 in numbers or num-1 in numbers:
             return True
     return False
 
+
+# Finds if numbers exists in the last lotto results
 def in_last(numbers, lotto_db):
     for num in numbers:
         if num in lotto_db[0]['numbers']:
             return True
     return False
 
+# Returns number of 2 following numbers in the lottery
+def calculate_two_following(lotto_db):
+    count = 0
+    for row in lotto_db:
+        if two_following(row['numbers']):
+            print row['numbers']
+            count += 1
+    return count
+
+# Returns number of appearence in the previous lotto results
+def calculate_in_last(lotto_db):
+    count = 0
+    last = {}
+    for row in lotto_db:
+        for i in range(1, 7):
+            if 'numbers' in last and int(row[str(i)]) in last['numbers']:
+                count += 1
+        last = row
+    return count
+
+
+# Preparations
 read_lotto_file()
 print most_recent_number(lotto_db=lotto_db)
-dump_db(lotto_db)
+# dump_db(lotto_db)
 sum_probability(lotto_db)
-#normal_sum_dist()
-#print number_ranges(lotto_db)
-#res = generate_results(100)
-#pres = generate_probability_results(100)
-#my_set = set()
-#for i in range(1,7):
-#    my_set.add(lotto_db[0][str(i)])
 
-#for i in range(1,100):
-#    print set(res[str(i)]['list']).intersection(set(pres[str(i)]['list']))
-#normal_sum_dist()
 
-#a = two_following([1,4,10,12,23,33])
-#b = in_last([1,4,10,12,23,33], lotto_db)
-count = 0
-for row in lotto_db:
-    if two_following(row['numbers']):
-        print row['numbers']
-        count+=1
-print count
-print len(lotto_db)
+final_results = []
+ranges = number_ranges(lotto_db)
+while len(final_results) < 10:
+    result = generate_results(ranges)
+    if two_following(result['list']) and result['Additional number'] < 81:
+        if in_last(result['list'], lotto_db):
+            if str(result['sum']) in ['105', '111', '97', '116', '100', '117', '114', '113', '104', '98']:
+                final_results.append(result)
+                print result
+
+
 
 
 
